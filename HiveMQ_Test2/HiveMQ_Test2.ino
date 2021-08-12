6cc11356efdad110519173377eb58efd36e99d5c
Yusuf Savaş <yusufsss4@gmail.com>
2021-08-12 16:52:30 +0300

    Fixed data

--- HiveMQ_Test2/HiveMQ_Test2.ino
+++ HiveMQ_Test2/HiveMQ_Test2.ino
@@ -1,30 +1,33 @@
 #include <Arduino.h>
+
+//MQTT and WiFi
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
+
+//Bluetooth Scan
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+#include <BLEBeacon.h>
+
 
 #define DATA_SEND 60000
 #include "MQTT_Config.h"
 
 long last_time = 0;
-//char data[100];
 
 WiFiClientSecure wifiClient;
 PubSubClient mqttClient(wifiClient);
 
 void connectToWiFi() {
-
   Serial.print("Connecting to..");
-
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.println(WIFI_SSID);
-
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
-
   Serial.print("Connected to the WiFi.");
-
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -33,6 +36,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
+  Serial.print("\n");
 }
 
 void setupMQTT() {
@@ -61,7 +65,7 @@ void reconnectToTheBroker() {
     if (mqttClient.connect(CLIENT_ID, MQTT_USER_NAME, MQTT_PASSWORD)) {
       Serial.println("Connected.");
       // subscribe to topic
-      mqttClient.subscribe("/home/esp32s/yusuf/commands");
+      mqttClient.subscribe("/o1/desk1/esp32-1/cm");
     }
     else {
       Serial.print("Connection failed, rc=");
@@ -82,36 +86,26 @@ void loop() {
   long now = millis();
   if (now - last_time > DATA_SEND) {
 
-    // Send data
-//    float temp = (float)random(0, 50); //bme.readTemperature();
-//    float hum = (float)random(0, 100); //bme.readHumidity();
-//    float pres = (float)random(0, 30); //bme.readPressure() / 100;
-
-
     // Publishing data through MQTT
     
-    //publish byte in terms of integer.
-    //uint8_t RSSIValue = 44;
-    //RSSIValue = 38;
-    //sprintf(data, "%d", RSSIValue);
-    //Serial.println(RSSIValue);
-    //mqttClient.publish("/home/esp32s/yusuf/temp", RSSIValue, 1);
-
     //publish byte.
-    int somethingElse = 123;
-    byte humidity[] = {0x82}; //130
-    humidity[0] = 123;
+    //int somethingElse = 123;
+    //byte RSSIValue[] = {0x82}; //130
+    //RSSIValue[0] = 123;
     //sprintf(data, "%d", humidity);
-    log_d("Humidity: \n");
-    Serial.write(humidity[0]);
-    mqttClient.publish("/home/esp32s/yusuf/humidity", humidity, 1); //1 byte.
+    //Serial.write(RSSIValue[0]);
+    //mqttClient.publish("/o1/m1/esp32-1/scn-dvc", RSSIValue, 1); //1 byte.
+
+    char RSSI_1[] = "123";
+    //sprintf(data, "%f", pres);
+    Serial.println(RSSI_1);
+    mqttClient.publish("/o1/m1/esp32-1/scn-dvc", RSSI_1);
     
     //publish string
-    char MACAdress[30] = "00:1B:44:11:3A:B7";
+    char MACAdress[] = "00:1B:44:11:3A:B7";
     //sprintf(data, "%f", pres);
-    log_d("MAC Adres: \n");
     Serial.println(MACAdress);
-    mqttClient.publish("/home/esp32s/yusuf/pressure", MACAdress);
+    mqttClient.publish("/o1/m1/esp32-1/info", MACAdress);
     last_time = now;
   }
 
