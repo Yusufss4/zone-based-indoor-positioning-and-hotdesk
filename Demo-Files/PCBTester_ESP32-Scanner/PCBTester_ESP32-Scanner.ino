2475a505f003cafe134e6ad24e59526d6fd3e691
Yusuf Savaş <yusufsss4@gmail.com>
2021-08-29 20:22:57 +0300

    Added some quick pcb test software

--- Demo-Files/PCBTester_ESP32-Scanner/PCBTester_ESP32-Scanner.ino
+++ Demo-Files/PCBTester_ESP32-Scanner/PCBTester_ESP32-Scanner.ino
@@ -2,26 +2,58 @@
 #define WIFI_LED 12 //12
 #define MQTT_LED 14 //14
 #define BLE_LED 27 //27
+#define BUTTON_PIN  26 //26
 
+boolean oldState = HIGH;
+int     mode     = 0;
 // The pin the LED is connected to
 void setup() {
   Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(WIFI_LED, OUTPUT);
   pinMode(MQTT_LED, OUTPUT);
   pinMode(BLE_LED, OUTPUT);// Declare the LED as an output
-    digitalWrite(WIFI_LED,LOW);
-  digitalWrite(MQTT_LED,LOW);
-  digitalWrite(BLE_LED,LOW);
+  digitalWrite(WIFI_LED, LOW);
+  digitalWrite(MQTT_LED, LOW);
+  digitalWrite(BLE_LED, LOW);
 }
 
 void loop() {
-  Serial.println("Led Test...");
-  delay(1000);
-  digitalWrite(WIFI_LED,LOW);
-  digitalWrite(MQTT_LED,LOW);
-  digitalWrite(BLE_LED,LOW);
-  delay(1000);
-  digitalWrite(WIFI_LED,HIGH);
-  digitalWrite(MQTT_LED,HIGH);
-  digitalWrite(BLE_LED,HIGH); // Turn the LED on
+  // Get current button state.
+  boolean newState = digitalRead(BUTTON_PIN);
+
+  // Check if state changed from high to low (button press).
+  if ((newState == LOW) && (oldState == HIGH)) {
+    // Short delay to debounce button.
+    delay(20);
+    // Check if button is still low after debounce.
+    newState = digitalRead(BUTTON_PIN);
+    if (newState == LOW) {     // Yes, still low
+      if (++mode > 2) mode = 0; // Advance to next mode, wrap around after #8
+      switch (mode) {          // Start the new animation...
+        case 0:
+          digitalWrite(WIFI_LED, HIGH);
+          digitalWrite(MQTT_LED, LOW);
+          digitalWrite(BLE_LED, LOW);
+          Serial.println("Case 0");
+          // Black/off
+          break;
+        case 1:
+          digitalWrite(WIFI_LED, LOW);
+          digitalWrite(MQTT_LED, HIGH);
+          digitalWrite(BLE_LED, LOW);   // Red
+          Serial.println("Case 1");
+          break;
+        case 2:
+          digitalWrite(WIFI_LED, LOW);
+          digitalWrite(MQTT_LED, LOW);
+          digitalWrite(BLE_LED, HIGH);  // Green
+          Serial.println("Case 2");
+          break;
+      }
+    }
+  }
+
+  // Set the last-read button state to the old state.
+  oldState = newState;
 }
