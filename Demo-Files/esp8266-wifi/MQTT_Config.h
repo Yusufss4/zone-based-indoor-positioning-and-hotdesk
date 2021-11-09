6a183fbf8cf4cab370dcd072495b2443d106c28d
Yusuf Savaş <yusufsss4@gmail.com>
2021-11-09 21:43:03 +0300

    Update MQTT_Config.h

--- Demo-Files/esp8266-wifi/MQTT_Config.h
+++ Demo-Files/esp8266-wifi/MQTT_Config.h
@@ -2,14 +2,14 @@
 #define MQTT_CONFIG_H
 
 /* WiFi setup Change Yours*/
-#define WIFI_SSID            "TURKSAT-KABLONET-4E7A-2.4G"
-#define WIFI_PASSWORD        "c42ecb6f"
+#define WIFI_SSID            "YOUR_WIFI_SSID"
+#define WIFI_PASSWORD        "YOUR_WIFI_PASSWORD"
 
 /* You can change but you dont need to*/
-#define MQTT_SERVER_NAME    "13.93.11.252"
+#define MQTT_SERVER_NAME    "YOUR_MQTT_SERVER_IP"
 #define MQTT_PORT    1883
-#define MQTT_USER_NAME "borda.academy:academy2021"
-#define MQTT_PASSWORD "academy2021"
+#define MQTT_USER_NAME "YOUR_MQTT_USER_NAME"
+#define MQTT_PASSWORD "YOUR_MQTT_PASSWORD"
 
 /* ALWAYS CHANGE!!*/
 #define CLIENT_ID "ESP8266-zeynep"
